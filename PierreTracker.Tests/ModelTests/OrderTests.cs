7c1ae00548f4b3164c33257d8b7e5f07edf5b5bb
Scott Hutley <scotthutley1@comcast.net>
2021-10-01 13:43:23 -0700

    Create class Order and OrderConstructor test

--- PierreTracker.Tests/ModelTests/OrderTests.cs
+++ PierreTracker.Tests/ModelTests/OrderTests.cs
@@ -8,6 +8,12 @@ namespace PierreTracker.Tests
   [TestClass]
   public class OrderTests
   {
-
+    
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 }
--- PierreTracker/Models/Order.cs
+++ PierreTracker/Models/Order.cs
@@ -4,6 +4,10 @@ namespace PierreTracker.Models
 {
   public class Order
   {
-    
+    public string OrderTitle { get; set; }
+    public string OrderDescription { get; set; }
+    public string OrderDate { get; set; }
+    public int OrderPrice { get; set; }
+    private static List<Order> _instances = new List<Order> {};
   }
 }
