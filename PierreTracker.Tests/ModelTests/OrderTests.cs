71b077172a191e2c603825b3721a034767a3fde9
Scott Hutley <scotthutley1@comcast.net>
2021-10-01 14:03:19 -0700

    Finish Order constructor, pass GetOrderTitle

--- PierreTracker.Tests/ModelTests/OrderTests.cs
+++ PierreTracker.Tests/ModelTests/OrderTests.cs
@@ -8,12 +8,21 @@ namespace PierreTracker.Tests
   [TestClass]
   public class OrderTests
   {
-    
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("test", "test", "test", "test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetOrderTitle_ReturnsOrderTitle_String()
+    {
+      string orderTitle = "Bread purchase";
+      Order newOrder = new Order(orderTitle, "test", "test", "test");
+      string result = newOrder.OrderTitle;
+      Assert.AreEqual(orderTitle, result);
     }
   }
 }
--- PierreTracker/Models/Order.cs
+++ PierreTracker/Models/Order.cs
@@ -7,7 +7,18 @@ namespace PierreTracker.Models
     public string OrderTitle { get; set; }
     public string OrderDescription { get; set; }
     public string OrderDate { get; set; }
-    public int OrderPrice { get; set; }
+    public string OrderPrice { get; set; }
+    public int Id { get; }
     private static List<Order> _instances = new List<Order> {};
+
+    public Order(string orderTitle, string orderDescription, string orderDate, string orderPrice)
+    {
+      OrderTitle = orderTitle;
+      OrderDescription = orderDescription;
+      OrderDate = orderDate;
+      OrderPrice = orderPrice;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
   }
 }
