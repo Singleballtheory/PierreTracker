31da9350cb0faae9ed9cf4456a8796df13a54f9b
Scott Hutley <scotthutley1@comcast.net>
2021-10-01 11:28:50 -0700

    add GetAll/ClearAll to Vendor, Dispose in test

--- PierreTracker/Models/Vendor.cs
+++ PierreTracker/Models/Vendor.cs
@@ -16,5 +16,15 @@ namespace PierreTracker.Models
       _instances.Add(this);
       Id = _instances.Count;
     }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
